b08ae442444107b819fa76d6e858c7ac00ecef21
jynoes <89066947+jynoes@users.noreply.github.com>
2023-11-01 11:45:30 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// SERVER SIDE LOGIC
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const mongoose = require("mongoose");
+const validator = require("validator");
+const fs = require("fs");
+
+const jwtSecret = process.env.JWT_SECRET;
+const app = express();
+const PORT = process.env.PORT || 5500;
+
+// Favicon
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+
+// MongoDB Connect
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (error) {}
+};
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on Port ${PORT}`);
+  });
+});
